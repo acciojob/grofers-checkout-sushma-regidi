1f4c8ca969e3dcacc83e8df09c06d416dde62861
sushma-regidi <regidisushma@gmail.com>
2025-11-18 13:00:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+
 function calculateTotal() {
-    const prices = document.querySelectorAll(".price");
+    const priceCells = document.querySelectorAll(".price");
     let total = 0;
 
-    prices.forEach(cell => {
-        let num = parseFloat(cell.textContent);
-        if (!isNaN(num)) total += num;
+    priceCells.forEach(cell => {
+        let value = parseFloat(cell.textContent.trim());
+        if (!isNaN(value)) total += value;
     });
 
     document.getElementById("ans").textContent = total;
 }
+
 
 
 
